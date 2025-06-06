c63a4866838eeee79738abb66bd1f381275a9ce9
Shubham <noreply+shubham@objectbox.io>
2025-06-06 12:24:52 +0530

    Rename model2VecSwiftWrapper to Model2Vec

--- Package.swift
+++ Package.swift
@@ -9,13 +9,13 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Model2Vec",
-            targets: ["model2vecSwiftWrapper"]),
+            targets: ["Model2Vec"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "model2vecSwiftWrapper",
+            name: "Model2Vec",
             dependencies: ["model2vecNativeLibs"]
         ),
         .binaryTarget(
